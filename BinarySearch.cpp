21cf9b091e675224abff611dc0e8cb313fb6678f
hermayani2786 <herma.yani2786@gmail.com>
2026-04-16 10:48:56 +0700

    menambahkan fungsi untuk menampilkan

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -50,3 +50,15 @@ void bubbleSortArray()
         pass++;
     } while (pass <= nPanjang - 1);
 }
+
+void display()
+{
+    cout << "\n=========================\n";
+    cout << "  Element Array Setelah Diurutkan (Asc)\n";
+    cout << "============================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        
+    }
+}
