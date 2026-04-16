e62855e2181745ece520847b5f6760121f999608
hermayani2786 <herma.yani2786@gmail.com>
2026-04-16 11:16:17 +0700

    memanggil semua fungsi

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -83,6 +83,41 @@ void binarySearch()
         int low = 0;
         int high = nPanjang - 1;
 
-    }
-    
+        do
+        {
+            int mid = (low + high) / 2;
+
+            if (element[mid] ==x)
+            {
+                cout << "\n[] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        
+        }while (low <= high);
+
+        if (low > high)
+        {
+            cout << "\n[x] Element " << x << " tidak ditemukan dalam array.\n";
+        }
+
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
+
+    }while (ulang == 'y' || ulang == 'y');
+}
+
+int main()
+{
+    input();
+    bubbleSortArray();
+    display();
+    binarySearch();
 }
